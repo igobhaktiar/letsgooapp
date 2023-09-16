5ddf5a7595cb8bd625904bd8dee1a1c964cf2ae3
Firgo Bhaktiar Hamsah <71805833+igobhaktiar@users.noreply.github.com>
2023-09-17 05:14:08 +0700

    row and column

--- letsgooapp/lib/FirstScreen.dart
+++ letsgooapp/lib/FirstScreen.dart
@@ -32,9 +32,97 @@ class FirstScreen extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: const Center(
-        child: Text('Hello Word!'),
+      body: const Padding (padding: EdgeInsets.all(16),
+        child:
+        Column(
+          children: <Widget> [
+            Text('MainAxisAligment.SpaceEvenly', style: TextStyle(fontSize: 18),),
+            Padding(padding: EdgeInsets.all(8)),
+            Row (
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget> [
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down)
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(8)),
+            Text('MainAxisAligment.SpaceEvenly', style: TextStyle(fontSize: 18),),
+            Padding(padding: EdgeInsets.all(8)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down)
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(8)),
+            Text('MainAxisAligment.spaceBetween', style: TextStyle(fontSize: 18),),
+            Padding(padding: EdgeInsets.all(8)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Icon(Icons.share),
+                Icon(Icons.thumb_up),
+                Icon(Icons.thumb_down)
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(8)),
+            Text('MainAxisAligment.star', style: TextStyle(fontSize: 18),),
+            Padding(padding: EdgeInsets.all(8)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(Icons.share),
+                SizedBox(width: 8),
+                Icon(Icons.thumb_up),
+                SizedBox(width: 8),
+                Icon(Icons.thumb_down)
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(8)),
+            Text('MainAxisAligment.end', style: TextStyle(fontSize: 18),),
+            Padding(padding: EdgeInsets.all(8)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Icon(Icons.share),
+                SizedBox(width: 8),
+                Icon(Icons.thumb_up),
+                SizedBox(width: 8),
+                Icon(Icons.thumb_down)
+              ],
+            ),
+          ],
+        )
+
       ),
+      //   decoration:  BoxDecoration(
+      //     color: Colors.red,
+      //     border: Border.all(color: Colors.green, width: 3),
+      //     borderRadius: BorderRadius.circular(10),
+      //     // shape: BoxShape.circle
+      //     boxShadow: const [
+      //       BoxShadow(
+      //           color: Colors.black,
+      //         offset: Offset(3, 6),
+      //         blurRadius: 10,
+      //       )
+      //     ]
+      //   ),
+      //   // width: 200,
+      //   // height: 100,
+      //   padding: const EdgeInsets.all(10),
+      //   margin: const EdgeInsets.all(10),
+      //   child: const Text('Hi',
+      //   style: TextStyle(fontSize: 40),
+      //   ),
+      // ),
+      //
+      // body: const Center(
+      //    child: Text('Hello Word!'),
+      // ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.delete),
         onPressed: (){},
